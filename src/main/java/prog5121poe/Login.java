a53488317811eed0d849098f329d384208b76563
Sandile <St10497257@rcconnect.edu.za>
2026-09-17 10:18:27 +0200

    Add Login class and user details

---
+++ src/main/java/prog5121poe/Login.java
@@ -0,0 +1,21 @@
+package prog5121poe;
+
+public class Login {
+
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+    private String firstName;
+    private String lastName;
+
+    public Login(String username, String password,
+                 String cellPhoneNumber,
+                 String firstName, String lastName) {
+
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
